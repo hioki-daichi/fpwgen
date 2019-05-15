6c45ad026550cfe27117ba3be6599ebf65fee8e1
Daichi Hioki <hiokidaichi@gmail.com>
2019-05-15 21:57:05 +0900

    [x] Use Number

--- src/Password.tsx
+++ src/Password.tsx
@@ -3,23 +3,33 @@ import axios from "axios";
 
 export const Password = () => {
   const [password, setPassword] = useState("");
+  const [useNumber, setUseNumber] = useState(false);
 
   useEffect(() => {
-    refreshPassword(setPassword);
-  }, []);
+    refreshPassword(setPassword, useNumber);
+  }, [useNumber]);
 
   return (
     <div>
       <h2>{password}</h2>
-      <button onClick={() => refreshPassword(setPassword)}>Refresh</button>
+      <label>
+        <input type="checkbox" onClick={() => setUseNumber(!useNumber)} />
+        Use Number
+      </label>
+      <div>
+        <button onClick={() => refreshPassword(setPassword, useNumber)}>
+          Refresh
+        </button>
+      </div>
     </div>
   );
 };
 
 const refreshPassword = async (
-  setPassword: Dispatch<SetStateAction<string>>
+  setPassword: Dispatch<SetStateAction<string>>,
+  useNumber: boolean
 ) => {
-  const res = await axios.post(process.env.REACT_APP_URL!, "{ password }");
+  const res = await axios.post(process.env.REACT_APP_URL!, `{ password(useNumber: ${useNumber}) }`);
 
   setPassword(res.data.data.password);
 };
